33431907a7c6edab94980a01194cde175f473cf9
aramirezdecruz3148 <leigh3148@gmail.com>
2019-10-26 10:21:06 -0700

    tryng to make a commit

--- Program.cs
+++ Program.cs
@@ -2,33 +2,37 @@
 
 namespace c_
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      String Username, Password = string.Empty;
+      Console.WriteLine("Welcome to de Cruz Bank");
+      Console.WriteLine("Please create an account");
+      Console.WriteLine("Enter a username");
+      Username = Console.ReadLine();
+      Console.WriteLine("Enter a password");
+      Password = Console.ReadLine();
+      Console.WriteLine("Thank you " + Username + " your account was created.");
+      Console.WriteLine("Press enter to continue...");
+      Console.ReadLine();
+      Console.Clear();
+      Console.WriteLine("To sign-in and begin banking please enter your username:");
+      var usernameInput = Console.ReadLine();
+      if (usernameInput == Username)
+      {
+        Console.WriteLine("Please enter your password");
+        var passwordInput = Console.ReadLine();
+        if (passwordInput == Password)
         {
-            String Username, Password = string.Empty;
-            Console.WriteLine("Welcome to de Cruz Bank");
-            Console.WriteLine("Please create an account");
-            Console.WriteLine("Enter a username");
-            Username = Console.ReadLine();
-            Console.WriteLine("Enter a password");
-            Password = Console.ReadLine();
-            Console.WriteLine("Thank you " + Username + " your account was created.");
-            Console.ReadLine();
-            Console.Clear();
-            Console.WriteLine("To sign-in and begin banking please enter your username:");
-            var usernameInput = Console.ReadLine();
-            if (usernameInput == Username) {
-                Console.WriteLine("Please enter your password");
-                var passwordInput = Console.ReadLine();
-                if(passwordInput == Password) {
-                    Console.WriteLine("Welcome " + Username + " you are logged in.");
-                }
-            }
-            else {
-                Console.WriteLine("I am sorry that was incorrect, press enter to try again");
-                var test = Console.ReadKey();
-            }
+          Console.WriteLine("Welcome " + Username + " you are logged in.");
         }
+      }
+      else
+      {
+        Console.WriteLine("I am sorry that was incorrect, press enter to try again");
+        var test = Console.ReadKey();
+      }
     }
+  }
 }
