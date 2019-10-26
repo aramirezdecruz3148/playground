b05f66dd13a527e2d959cceaeb30153b40ef5c6c
aramirezdecruz3148 <leigh3148@gmail.com>
2019-10-26 11:11:34 -0700

    playing with adding numbers

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace c_
     static void Main(string[] args)
     {
       String Username, Password = string.Empty;
+      Int32 Balance = 0;
       Console.WriteLine("Welcome to de Cruz Bank");
       Console.WriteLine("Please create an account");
       Console.WriteLine("Enter a username");
@@ -14,7 +15,7 @@ namespace c_
       Console.WriteLine("Enter a password");
       Password = Console.ReadLine();
       Console.WriteLine("Thank you " + Username + " your account was created.");
-      Console.WriteLine("Press enter to continue...");
+      Console.WriteLine("Press < enter > to continue...");
       Console.ReadLine();
       Console.Clear();
       Console.WriteLine("To sign-in and begin banking please enter your username:");
@@ -26,6 +27,11 @@ namespace c_
         if (passwordInput == Password)
         {
           Console.WriteLine("Welcome " + Username + " you are logged in.");
+          Console.WriteLine("To make an initial deposit, please enter the amount");
+          var deposit = Console.ReadLine();
+          var actualDeposit = Int32.Parse(deposit);
+          var newBalance = actualDeposit + Balance;
+          Console.WriteLine("Thank you for your deposit, you now have " + newBalance + " in your account");
         }
       }
       else
