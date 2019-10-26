e5095a21de41e9113671dac13c917de2d9aedbdb
aramirezdecruz3148 <leigh3148@gmail.com>
2019-10-26 16:10:40 -0700

    now playing with a switch statement to create a kind of menu

--- Program.cs
+++ Program.cs
@@ -6,38 +6,19 @@ namespace c_
   {
     static void Main(string[] args)
     {
-      String Username, Password = "";
-      var Balance = 0;
-      Console.WriteLine("Welcome to de Cruz Bank");
-      Console.WriteLine("Please create an account");
-      Console.WriteLine("Enter a username");
-      Username = Console.ReadLine();
-      Console.WriteLine("Enter a password");
-      Password = Console.ReadLine();
-      Console.WriteLine("Thank you " + Username + " your account was created.");
-      Console.WriteLine("Press < enter > to continue...");
-      Console.ReadLine();
-      Console.Clear();
-      Console.WriteLine("To sign-in and begin banking please enter your username:");
-      var usernameInput = Console.ReadLine();
-      if (usernameInput == Username)
+      Console.WriteLine("Press 1 for deposit, press 2 for withdrawal, 3 for balance: ");
+      var action = int.Parse(Console.ReadLine());
+      switch (action)
       {
-        Console.WriteLine("Please enter your password");
-        var passwordInput = Console.ReadLine();
-        if (passwordInput == Password)
-        {
-          Console.WriteLine("Welcome " + Username + " you are logged in.");
-          Console.WriteLine("To make an initial deposit, please enter the amount");
-          var deposit = Console.ReadLine();
-          var actualDeposit = Decimal.Parse(deposit);
-          var newBalance = actualDeposit + Balance;
-          Console.WriteLine("Thank you for your deposit, you now have " + newBalance + " in your account");
-        }
-      }
-      else
-      {
-        Console.WriteLine("I am sorry that was incorrect, press enter to try again");
-        var test = Console.ReadKey();
+          case 1:
+              Console.WriteLine("Case 1");
+              break;
+          case 2:
+              Console.WriteLine("Case 2");
+              break;
+          default:
+              Console.WriteLine("Default case");
+              break;
       }
     }
   }
