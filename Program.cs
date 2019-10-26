9d7e8c0fa35b700c46aa9f871c726cb82a817b55
aramirezdecruz3148 <leigh3148@gmail.com>
2019-10-26 16:55:27 -0700

    able to write to a file in the code base

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace c_
 {
@@ -6,20 +7,10 @@ namespace c_
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Press 1 for deposit, press 2 for withdrawal, 3 for balance: ");
-      var action = int.Parse(Console.ReadLine());
-      switch (action)
-      {
-          case 1:
-              Console.WriteLine("Case 1");
-              break;
-          case 2:
-              Console.WriteLine("Case 2");
-              break;
-          case 3:
-              Console.WriteLine("Case 3");
-              break;
-      }
+     using(StreamWriter writetext = new StreamWriter("write.txt", true))
+{
+    writetext.WriteLine("writing in text file");
+}
     }
   }
 }
