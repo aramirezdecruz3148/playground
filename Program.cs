6c0d22bc0f9b51845c960113c2d0de86f1eed27f
aramirezdecruz3148 <leigh3148@gmail.com>
2019-10-27 16:33:19 -0700

    added the Newtonsoft.Json package and playing with it, managed to get it to write to a json file within the build, now going to work on formatting it

--- Program.cs
+++ Program.cs
@@ -1,16 +1,33 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.Serialization.Json;
+using Newtonsoft.Json;
 
 namespace c_
 {
+  public class Movie
+{
+    public string Name { get; set; }
+    public int Year { get; set; }
+}
   class Program
   {
     static void Main(string[] args)
     {
-     using(StreamWriter writetext = new StreamWriter("write.txt", true))
-{
-    writetext.WriteLine("writing in text file");
-}
+      Movie movie = new Movie
+      {
+          Name = "Bad Boys",
+          Year = 1995
+      };
+      // serialize JSON to a string and then write string to a file
+      File.WriteAllText(@"c:\movie.json", JsonConvert.SerializeObject(movie));
+
+      // serialize JSON directly to a file
+      using (StreamWriter file = File.CreateText(@"c:\movie.json"))
+      {
+          JsonSerializer serializer = new JsonSerializer();
+          serializer.Serialize(file, movie);
+      }
     }
   }
 }
