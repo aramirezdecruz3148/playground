f4a46d60665544cbede4435bce1d0baa95aa8984
aramirezdecruz3148 <leigh3148@gmail.com>
2019-10-28 09:11:54 -0700

    starting with a clean slate to play around with building a simple version of a program

--- Program.cs
+++ Program.cs
@@ -1,33 +1,9 @@
 ﻿using System;
-using System.IO;
-using System.Runtime.Serialization.Json;
-using Newtonsoft.Json;
 
-namespace c_
-{
-  public class Movie
-{
-    public string Name { get; set; }
-    public int Year { get; set; }
-}
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      Movie movie = new Movie
-      {
-          Name = "Bad Boys",
-          Year = 1995
-      };
-      // serialize JSON to a string and then write string to a file
-      File.WriteAllText(@"c:\movie.json", JsonConvert.SerializeObject(movie));
+namespace c_ {
+  class Program {
+    static void Main(string[] args) {
 
-      // serialize JSON directly to a file
-      using (StreamWriter file = File.CreateText(@"c:\movie.json"))
-      {
-          JsonSerializer serializer = new JsonSerializer();
-          serializer.Serialize(file, movie);
-      }
     }
   }
 }
